410faa376d3162f0ba7b360623fa05ccc3b954b0
HansenDafa <hansen.pramudya@gmail.com>
2024-12-19 23:29:43 +0700

    fix dashboard mahasiswa part ii

--- resources/views/dashboard/mahasiswa/index.blade.php
+++ resources/views/dashboard/mahasiswa/index.blade.php
@@ -95,3 +95,101 @@
                     </div>
                 </div>
             </div>
+
+            <div class="row mt-4">
+                <div class="col-lg-7 mb-lg-0 mb-4">
+                    <div class="card">
+                        <div class="card-header pb-0">
+                            <h6>Konsultasi Terakhir</h6>
+                        </div>
+                        <div class="card-body p-3">
+                            <div class="timeline timeline-one-side">
+                                @foreach ($latestConsultations as $consultation)
+                                    <div class="timeline-block mb-3">
+                                        <span class="timeline-step">
+                                            <i class="ni ni-bell-55 text-success text-gradient"></i>
+                                        </span>
+                                        <div class="timeline-content">
+                                            <h6 class="text-dark text-sm font-weight-bold mb-0">
+                                                {{ $consultation->superVision->dosen->user->name }}</h6>
+                                            <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">
+                                                {{ Carbon\Carbon::parse($consultation->created_at)->diffForHumans() }}
+                                            </p>
+                                            <p class="text-sm mt-3 mb-2">
+                                                {{ Str::limit($consultation->notes, 100) }}
+                                            </p>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5">
+                    <div class="card">
+                        <div class="card-header pb-0">
+                            <h6>Jadwal Konsultasi Mendatang</h6>
+                        </div>
+                        <div class="card-body p-3">
+                            <ul class="list-group">
+                                @foreach ($upcomingAppointments as $appointment)
+                                    <li
+                                        class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                                        <div class="d-flex align-items-center">
+                                            <div
+                                                class="icon icon-shape icon-sm me-3 bg-gradient-dark shadow text-center">
+                                                <i class="ni ni-mobile-button text-white opacity-10"></i>
+                                            </div>
+                                            <div class="d-flex flex-column">
+                                                <h6 class="mb-1 text-dark text-sm">
+                                                    {{ $appointment->schedule->dosen->user->name }}</h6>
+                                                <span
+                                                    class="text-xs">{{ Carbon\Carbon::parse($appointment->schedule->date)->format('d M Y') }}
+                                                    - ,
+                                                    {{ $appointment->schedule->start_time->format('H:i') }} -
+                                                    {{ $appointment->schedule->end_time->format('H:i') }}</span>
+                                            </div>
+                                        </div>
+                                        <div class="d-flex">
+                                            <button
+                                                class="btn btn-link btn-icon-only btn-rounded btn-sm text-dark icon-move-right my-auto"><i
+                                                    class="ni ni-bold-right" aria-hidden="true"></i></button>
+                                        </div>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mt-4">
+                <div class="col-lg-12 mb-lg-0 mb-4">
+                    <div class="card">
+                        <div class="card-header pb-0">
+                            <h6>Dosen Pembimbing</h6>
+                        </div>
+                        <div class="card-body p-3">
+                            <div class="row">
+                                @foreach ($supervisionStatus as $supervision)
+                                    <div class="col-md-6 mb-4">
+                                        <div
+                                            class="card card-body border card-plain border-radius-lg d-flex align-items-center flex-row">
+                                            <img class="w-10 me-3 mb-0" src="{{ asset('img/team-2.jpg') }}"
+                                                alt="logo">
+                                            <h6 class="mb-0">{{ $supervision->dosen->user->name }}</h6>
+                                            <span
+                                                class="ms-auto badge badge-sm bg-gradient-{{ $supervision->status == 'approved' ? 'success' : 'warning' }}">{{ ucfirst($supervision->status) }}</span>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </main>
+</x-layout>
